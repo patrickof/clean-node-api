5c64f319b8aa4018db1437497189baf2331f7299
Patrick Feitosa <patrickofboy@hotmail.com>
2022-02-08 22:44:53 -0300

    move production code to their own files

---
+++ src/presentation/routers/login-router.js
@@ -0,0 +1,19 @@
+const HttpResponse = require('../helpers/http-response')
+
+module.exports = class LoginRouter {
+  rote (httpRequest) {
+    if (!httpRequest || !httpRequest.body) {
+      return HttpResponse.serverError()
+    }
+
+    const { email, password } = httpRequest.body
+
+    if (!email) {
+      return HttpResponse.badRequest('email')
+    }
+
+    if (!password) {
+      return HttpResponse.badRequest('password')
+    }
+  }
+}

9fb6cebfbdb85aeb9dfb3cb01c6309a0e18feb12
Patrick Feitosa <patrickofboy@hotmail.com>
2022-12-03 00:16:47 -0300

    simplify code

--- src/domain/usecases/auth-usecase.js
+++ src/domain/usecases/auth-usecase.js
@@ -17,17 +17,13 @@ class AuthUseCase {
     }
 
     const user = await this.loadUserByEmailRepository.load(email)
+    const isValid = user && await this.encrypter.compare(user.password, password)
 
-    if (!user) {
-      return null
+    if (isValid) {
+      const accessToken = await this.tokenGenerator.generate(user.id)
+      return accessToken
     }
-    const isValid = await this.encrypter.compare(user.password, password)
-
-    if (!isValid) {
-      return null
-    }
-    const accessToken = await this.tokenGenerator.generate(user.id)
-    return accessToken
+    return null
   }
 }
 
